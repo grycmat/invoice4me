e7c5db75536b139264df9abdcfcea6e39b7ac802
Mateusz Gryc <mateusz@gryc.dev>
2025-09-05 10:37:20 +0200

    working on nev invoice template

--- app/src/main/java/com/gigapingu/invoice4me/PdfPrinter.kt
+++ app/src/main/java/com/gigapingu/invoice4me/PdfPrinter.kt
@@ -1,75 +1,28 @@
 package com.gigapingu.invoice4me
 
-import android.graphics.Picture
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.Canvas
-import androidx.compose.ui.graphics.drawscope.draw
+import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.core.graphics.createBitmap
 
 @Composable
 fun PdfPrinter(
     modifier: Modifier = Modifier
 ) {
-    val ctx = LocalContext.current
-    var picture: Picture? by remember { mutableStateOf(null) }
-    val launcher =
-        rememberLauncherForActivityResult(ActivityResultContracts.CreateDocument("image/png")) { uri ->
-            if (uri != null) {
-                ctx.contentResolver.openOutputStream(uri).use {
-                    if (it != null && picture != null) {
-                        val bitmap = createBitmap(picture!!.width, picture!!.height)
-                        val canvas = android.graphics.Canvas(bitmap)
-                        picture!!.draw(canvas)
-                        bitmap.compress(android.graphics.Bitmap.CompressFormat.PNG, 100, it)
-                        bitmap.recycle()
-                    }
-                }
-            }
-        }
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(modifier = Modifier.padding(vertical = 100.dp), onClick = {
-                launcher.launch("saved_image.png")
-            }) {
-                Text("PDF")
-            }
-        }
-    ) { innerPadding ->
-        Box(
-            modifier = modifier
-                .padding(innerPadding)
-                .drawWithContent {
-                    val width = 500
-                    val height = 700
-                    val pic = Picture()
-                    val canvas = Canvas(pic.beginRecording(width, height))
-                    draw(this, this.layoutDirection, canvas, this.size) {
-                        this@drawWithContent.drawContent()
-                    }
-                    pic.endRecording()
-                    picture = pic
-                    drawContent()
-                }
-        ) {
+    val composeView = ComposeView(LocalContext.current).apply {
+        setContent {
             TempInvoicePdf()
         }
     }
+        composeView.measure(0, 0)
+    Box(
+        modifier = modifier.wrapContentSize()
+    ) {
+    }
+
 }
 
 @Preview(name = "PdfPrinter")
--- app/src/main/java/com/gigapingu/invoice4me/ui/screens/MainScreen.kt
+++ app/src/main/java/com/gigapingu/invoice4me/ui/screens/MainScreen.kt
@@ -1,12 +1,18 @@
 package com.gigapingu.invoice4me.ui.screens
 
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
+import com.gigapingu.invoice4me.TempInvoicePdf2
 import com.gigapingu.invoice4me.model.InvoiceItem
 import com.gigapingu.invoice4me.navigation.LocalNavController
 import com.gigapingu.invoice4me.ui.navigation.AppNavigation
@@ -19,18 +25,22 @@ fun MainScreen() {
 
 
     var editingItem by remember { mutableStateOf<InvoiceItem?>(null) }
+    val scroll = rememberScrollState()
 
-    CompositionLocalProvider(LocalNavController provides navController)  {
-        Invoice4MeTheme {
-            Scaffold(
-                modifier = Modifier.fillMaxSize(),
-                containerColor = Color.Transparent,
-                bottomBar = { AppNavigation(navController) }
-            ) { innerPadding ->
-                NavigationHost(navController, innerPadding, editingItem)
-            }
-        }
+    Column(modifier = Modifier.fillMaxSize().verticalScroll(scroll)) {
+        TempInvoicePdf2()
     }
+//    CompositionLocalProvider(LocalNavController provides navController)  {
+//        Invoice4MeTheme {
+//            Scaffold(
+//                modifier = Modifier.fillMaxSize(),
+//                containerColor = Color.Transparent,
+//                bottomBar = { AppNavigation(navController) }
+//            ) { innerPadding ->
+//                NavigationHost(navController, innerPadding, editingItem)
+//            }
+//        }
+//    }
 }
 
 
